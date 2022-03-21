ffbc40f083cd5866ca92ee32d059163522340430
Puanqq <quang.huuvinhnguyen@infodation.vn>
2022-03-21 16:52:00 +0700

    commit checkout feature with new tools
    + Filters
    + Middlewares
    + change all to OrderManager

---
+++ Checkout.API/DTOs/OrderDetailDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Checkout.API.DTOs
+{
+    public class OrderDetailDto
+    {
+        public Guid Id { get; set; }
+        public Guid OrderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ Checkout.API/DTOs/OrderDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Checkout.API.DTOs
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public double Total { get; set; }
+        public DateTime CreateAt { get; set; }
+        public List<OrderDetailDto>? OrderDetails { get; set; }
+    }
+}
